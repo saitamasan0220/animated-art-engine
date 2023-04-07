4a9188e3e1383b0dd5c18f630b7aef130a06de60
saitamasan0220 <103046751+saitamasan0220@users.noreply.github.com>
2023-04-07 06:41:43 -0400

    update configs

--- step2_spritesheet_to_generative_sheet/src/config.js
+++ step2_spritesheet_to_generative_sheet/src/config.js
@@ -14,119 +14,224 @@ const buildDir = path.join(basePath, "../build"); // JSON are written to json fo
  ***********************/
 
 const {
-  numFramesPerBatch,
-  numberOfFrames,
-  useBatches,
-  description,
-  baseUri,
-  height,
-  width,
-  startIndex,
-  thumbnailUri,
-  generateThumbnail,
-  debug,
-  totalSupply,
-  layersFolder,
-  outputType,
-  animationUri
-} = require(path.join(
-  basePath,
-  "../global_config.json"
-));
+    numFramesPerBatch,
+    numberOfFrames,
+    useBatches,
+    description,
+    baseUri,
+    height,
+    width,
+    startIndex,
+    thumbnailUri,
+    generateThumbnail,
+    debug,
+    totalSupply,
+    layersFolder,
+    outputType,
+    animationUri,
+} = require(path.join(basePath, "../global_config.json"));
 
 /* ONLY VARIABLE THAT YOU NEED TO EDIT IS HERE */
 let layerConfigurations = [
-  {
-    growEditionSizeTo: totalSupply,
-    namePrefix: "", // Use to add a name to Metadata `name:`
-    layersOrder: [
-      { name: "Background" },
-      { name: "Landscape" },
-      {
-        name: "Ball",
-      },
-      {
-        name: "Hat",
-      },
-    ],
-  },
-]
+    {
+        growEditionSizeTo: totalSupply - Math.floor(totalSupply / 12) * 11,
+        namePrefix: "Baptized",
+        layersOrder: [
+            { name: "Background" },
+            { name: "Asher Bible", trait: "Bible" },
+            { name: "Asher Crosses", trait: "Cross" },
+            { name: "Verse" },
+        ],
+    },
+    {
+        growEditionSizeTo: totalSupply - Math.floor(totalSupply / 12) * 10,
+        namePrefix: "Baptized",
+        layersOrder: [
+            { name: "Background" },
+            { name: "Benjamin Bible", trait: "Bible" },
+            { name: "Benjamin Crosses", trait: "Cross" },
+            { name: "Verse" },
+        ],
+    },
+    {
+        growEditionSizeTo: totalSupply - Math.floor(totalSupply / 12) * 9,
+        namePrefix: "Baptized",
+        layersOrder: [
+            { name: "Background" },
+            { name: "Gad Bible", trait: "Bible" },
+            { name: "Gad Crosses", trait: "Cross" },
+            { name: "Verse" },
+        ],
+    },
+    {
+        growEditionSizeTo: totalSupply - Math.floor(totalSupply / 12) * 8,
+        namePrefix: "Baptized",
+        layersOrder: [
+            { name: "Background" },
+            { name: "Issachar Bible", trait: "Bible" },
+            { name: "Issachar Crosses", trait: "Cross" },
+            { name: "Verse" },
+        ],
+    },
+    {
+        growEditionSizeTo: totalSupply - Math.floor(totalSupply / 12) * 7,
+        namePrefix: "Baptized",
+        layersOrder: [
+            { name: "Background" },
+            { name: "Joseph Bible", trait: "Bible" },
+            { name: "Joseph Crosses", trait: "Cross" },
+            { name: "Verse" },
+        ],
+    },
+    {
+        growEditionSizeTo: totalSupply - Math.floor(totalSupply / 12) * 6,
+        namePrefix: "Baptized",
+        layersOrder: [
+            { name: "Background" },
+            { name: "Judah Bible", trait: "Bible" },
+            { name: "Judah Crosses", trait: "Cross" },
+            { name: "Verse" },
+        ],
+    },
+    {
+        growEditionSizeTo: totalSupply - Math.floor(totalSupply / 12) * 5,
+        namePrefix: "Baptized",
+        layersOrder: [
+            { name: "Background" },
+            { name: "Levi Bible", trait: "Bible" },
+            { name: "Levi Crosses", trait: "Cross" },
+            { name: "Verse" },
+        ],
+    },
+    {
+        growEditionSizeTo: totalSupply - Math.floor(totalSupply / 12) * 4,
+        namePrefix: "Baptized",
+        layersOrder: [
+            { name: "Background" },
+            { name: "Manasseh Bible", trait: "Bible" },
+            { name: "Manasseh Crosses", trait: "Cross" },
+            { name: "Verse" },
+        ],
+    },
+    {
+        growEditionSizeTo: totalSupply - Math.floor(totalSupply / 12) * 3,
+        namePrefix: "Baptized",
+        layersOrder: [
+            { name: "Background" },
+            { name: "Naphtali Bible", trait: "Bible" },
+            { name: "Naphtali Crosses", trait: "Cross" },
+            { name: "Verse" },
+        ],
+    },
+    {
+        growEditionSizeTo: totalSupply - Math.floor(totalSupply / 12) * 2,
+        namePrefix: "Baptized",
+        layersOrder: [
+            { name: "Background" },
+            { name: "Reuben Bible", trait: "Bible" },
+            { name: "Reuben Crosses", trait: "Cross" },
+            { name: "Verse" },
+        ],
+    },
+    {
+        growEditionSizeTo: totalSupply - Math.floor(totalSupply / 12) * 1,
+        namePrefix: "Baptized",
+        layersOrder: [
+            { name: "Background" },
+            { name: "Simeon Bible", trait: "Bible" },
+            { name: "Simeon Crosses", trait: "Cross" },
+            { name: "Verse" },
+        ],
+    },
+    {
+        growEditionSizeTo: totalSupply - Math.floor(totalSupply / 12) * 0,
+        namePrefix: "Baptized",
+        layersOrder: [
+            { name: "Background" },
+            { name: "Zebulun Bible", trait: "Bible" },
+            { name: "Zebulun Crosses", trait: "Cross" },
+            { name: "Verse" },
+        ],
+    },
+];
 
 const format = {
-  width: useBatches ? (width * numFramesPerBatch) : (width * numberOfFrames),
-  height,
-  smoothing: true, // set to false when up-scaling pixel art.
+    width: useBatches ? width * numFramesPerBatch : width * numberOfFrames,
+    height,
+    smoothing: true, // set to false when up-scaling pixel art.
 };
 
 const background = {
-  generate: false,
-  brightness: "80%",
+    generate: false,
+    brightness: "80%",
 };
 
-
 const layerConfigurationsZIndex = [
-  {
-    growEditionSizeTo: totalSupply,
-    namePrefix: "Bouncing Ball Z-Index Example:",
-    layersOrder: [
-      { name: "Background" },
-      { name: "Landscape" },
-      { name: "Ball" },
-    ],
-  },
-]
+    {
+        growEditionSizeTo: totalSupply,
+        namePrefix: "Bouncing Ball Z-Index Example:",
+        layersOrder: [
+            { name: "Background" },
+            { name: "Landscape" },
+            { name: "Ball" },
+        ],
+    },
+];
 
 // This will create totalSupply - 1 common balls, and 1 rare ball
 // They will be in order but you can shuffleLayerConfigurations
 const layerConfigurationsGrouping = [
-  {
-    growEditionSizeTo: totalSupply - 1,
-    namePrefix: "Bouncing Ball Common:",
-    layersOrder: [
-      { name: "Background" },
-      { name: "Landscape" },
-      { name: "Common Ball", trait: "Ball" },
-      { name: "Common Hat", trait: "Hat" },
-    ],
-  },
-  {
-    growEditionSizeTo: totalSupply,
-    namePrefix: "Bouncing Ball Rare:",
-    layersOrder: [
-      { name: "Background" },
-      { name: "Landscape" },
-      { name: "Rare Ball", trait: "Ball" },
-      { name: "Rare Hat", trait: "Hat" },
-    ],
-  },
-]
+    {
+        growEditionSizeTo: totalSupply - 1,
+        namePrefix: "Bouncing Ball Common:",
+        layersOrder: [
+            { name: "Background" },
+            { name: "Landscape" },
+            { name: "Common Ball", trait: "Ball" },
+            { name: "Common Hat", trait: "Hat" },
+        ],
+    },
+    {
+        growEditionSizeTo: totalSupply,
+        namePrefix: "Bouncing Ball Rare:",
+        layersOrder: [
+            { name: "Background" },
+            { name: "Landscape" },
+            { name: "Rare Ball", trait: "Ball" },
+            { name: "Rare Hat", trait: "Hat" },
+        ],
+    },
+];
 
 const layerConfigurationsIfThen = [
-  {
-    growEditionSizeTo: totalSupply,
-    namePrefix: "", // Use to add a name to Metadata `name:`
-    layersOrder: [
-      { name: "Background" },
-      { name: "Landscape" },
-      {
-        name: "Ball",
-      },
-    ],
-  },
-]
+    {
+        growEditionSizeTo: totalSupply,
+        namePrefix: "", // Use to add a name to Metadata `name:`
+        layersOrder: [
+            { name: "Background" },
+            { name: "Landscape" },
+            {
+                name: "Ball",
+            },
+        ],
+    },
+];
 
 const handler = {
-  get: function (target, name) {
-    return target.hasOwnProperty(name) ? target[name] : layerConfigurations;
-  }
+    get: function (target, name) {
+        return target.hasOwnProperty(name) ? target[name] : layerConfigurations;
+    },
 };
 
-const layerConfigurationMapping = new Proxy({
-  "layers": layerConfigurations,
-  "layers_z_index": layerConfigurationsZIndex,
-  "layers_grouping": layerConfigurationsGrouping,
-  "layers_if_then": layerConfigurationsIfThen,
-}, handler);
+const layerConfigurationMapping = new Proxy(
+    {
+        layers: layerConfigurations,
+        layers_z_index: layerConfigurationsZIndex,
+        layers_grouping: layerConfigurationsGrouping,
+        layers_if_then: layerConfigurationsIfThen,
+    },
+    handler
+);
 
 layerConfigurations = layerConfigurationMapping[layersFolder];
 
@@ -134,7 +239,7 @@ layerConfigurations = layerConfigurationMapping[layersFolder];
  * Set to true for when using multiple layersOrder configuration
  * and you would like to shuffle all the artwork together
  */
-const shuffleLayerConfigurations = false;
+const shuffleLayerConfigurations = true;
 
 const debugLogs = debug;
 
@@ -156,7 +261,7 @@ const emptyLayerName = "NONE";
  * will not have the flashing ball
  */
 const incompatible = {
-  // Flashing: ["Multicolor"],
+    // Flashing: ["Multicolor"],
 };
 
 /**
@@ -169,7 +274,7 @@ const incompatible = {
  * further in the stack
  */
 const forcedCombinations = {
-  // floral: ["MetallicShades", "Golden Sakura"],
+    // floral: ["MetallicShades", "Golden Sakura"],
 };
 
 /**
@@ -178,35 +283,35 @@ const forcedCombinations = {
  * clean-filename: trait-value override pairs. Wrap filenames with spaces in quotes.
  */
 const traitValueOverrides = {
-  // Helmet: "Space Helmet",
-  // "gold chain": "GOLDEN NECKLACE",
+    // Helmet: "Space Helmet",
+    // "gold chain": "GOLDEN NECKLACE",
 };
 
 const extraMetadata = {};
 
 const extraAttributes = () => [
-  // Optionally, if you need to overwrite one of your layers attributes.
-  // You can include the same name as the layer, here, and it will overwrite
-  //
-  // {
-  // trait_type: "Bottom lid",
-  //   value: ` Bottom lid # ${Math.random() * 100}`,
-  // },
-  // {
-  //   display_type: "boost_number",
-  //   trait_type: "Aqua Power",
-  //   value: Math.random() * 100,
-  // },
-  // {
-  //   display_type: "boost_number",
-  //   trait_type: "Health",
-  //   value: Math.random() * 100,
-  // },
-  // {
-  //   display_type: "boost_number",
-  //   trait_type: "Mana",
-  //   value: Math.floor(Math.random() * 100),
-  // },
+    // Optionally, if you need to overwrite one of your layers attributes.
+    // You can include the same name as the layer, here, and it will overwrite
+    //
+    // {
+    // trait_type: "Bottom lid",
+    //   value: ` Bottom lid # ${Math.random() * 100}`,
+    // },
+    // {
+    //   display_type: "boost_number",
+    //   trait_type: "Aqua Power",
+    //   value: Math.random() * 100,
+    // },
+    // {
+    //   display_type: "boost_number",
+    //   trait_type: "Health",
+    //   value: Math.random() * 100,
+    // },
+    // {
+    //   display_type: "boost_number",
+    //   trait_type: "Mana",
+    //   value: Math.floor(Math.random() * 100),
+    // },
 ];
 
 const rarityDelimiter = "#";
@@ -221,28 +326,28 @@ const uniqueDnaTorrance = 10000;
 const useRootTraitType = true;
 
 module.exports = {
-  animationUri,
-  background,
-  baseUri,
-  buildDir,
-  debugLogs,
-  description,
-  emptyLayerName,
-  extraAttributes,
-  extraMetadata,
-  forcedCombinations,
-  format,
-  generateThumbnail,
-  incompatible,
-  layerConfigurations,
-  layersDir,
-  outputDir,
-  thumbnailUri,
-  rarityDelimiter,
-  shuffleLayerConfigurations,
-  startIndex,
-  traitValueOverrides,
-  uniqueDnaTorrance,
-  useRootTraitType,
-  outputType,
+    animationUri,
+    background,
+    baseUri,
+    buildDir,
+    debugLogs,
+    description,
+    emptyLayerName,
+    extraAttributes,
+    extraMetadata,
+    forcedCombinations,
+    format,
+    generateThumbnail,
+    incompatible,
+    layerConfigurations,
+    layersDir,
+    outputDir,
+    thumbnailUri,
+    rarityDelimiter,
+    shuffleLayerConfigurations,
+    startIndex,
+    traitValueOverrides,
+    uniqueDnaTorrance,
+    useRootTraitType,
+    outputType,
 };
