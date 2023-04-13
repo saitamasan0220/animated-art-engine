4933d4d4f92c0ff125ec55ba30b4567ab0204095
saitamasan0220 <103046751+saitamasan0220@users.noreply.github.com>
2023-04-13 09:44:45 -0400

    annoying

--- script_count.py
+++ script_count.py
@@ -4,20 +4,27 @@ from random import shuffle
 import random
 
 
+# table = {}
+# path='build/json/'
+# for filename in os.listdir(path):
+#     print(f'saving {filename} in table')
+#     with open(path+filename, 'r') as file:
+#         file_text = file.read()
+#         table[filename] = file_text
 
-path='build/json/'
-for filename in os.listdir(path):
-    print(f'saving {filename} in table')
-    with open(path+filename, 'r') as file:
-        file_text = file.read()
-        table[filename] = file_text
+indexes = list(range(1, 11989))
 
-indexes = list(range(1, 19886))
-
+# count = 0
 with os.scandir('build/json/') as directory:
     for item in directory:
         if item.is_file():
             index = item.name.replace(".json", "")
-            indexes.remove(index)
+            int_index = int(index)
+            print("index: ", int_index)
+            indexes.remove(int_index)
+            print("# remaining indexes: ", len(indexes))
+            # count += 1 
     
 print("remaining indexes: ", indexes)
+print("# remaining indexes: ", len(indexes))
+# print("count: ", count)
