3f92c3ffa73d8814304c52f3a774fbadc7f0935c
saitamasan0220 <103046751+saitamasan0220@users.noreply.github.com>
2023-04-14 11:33:32 -0400

    Update script_update.py

--- script_update.py
+++ script_update.py
@@ -4,17 +4,30 @@ import re
 
 with os.scandir('build/json/') as directory:
     for item in directory:
-        if item.is_file() and int(item.name.replace('.json', '')) in random_map:
+        if item.is_file():
             with open(item, mode="r+") as file:
 
                 file_text = file.read()
-                regex = re.compile('\d+.gif')
-                new_name = item.name.replace('.json', '.gif')
-                file_text = regex.sub(f'{new_name}', file_text)
 
-                regex = re.compile('#\d+')
-                new_name = '#' + item.name.replace('.json', '')
-                file_text = regex.sub(f'{new_name}', file_text)
+                regex = re.compile('WEBUriToReplace')
+                new_text_uri =
+                file_text = regex.sub(f'{new_text_uri}', file_text)
+
+                id = int(item.name.replace('.json', ''))
+
+                regex = re.compile('NewUriToReplace')
+
+                if 1 <= id <= 3000:  # q1
+                    new_folder_uri =
+                elif 3001 <= id <= 6000:  # q2
+                    new_folder_uri =
+                elif 1 <= id <= 3000:  # q3
+                    new_folder_uri =
+                else:  # q4
+                    new_folder_uri =
+                file_text = regex.sub(f'{new_folder_uri}', file_text)
+
+                print("new_folder_uri: ", new_folder_uri)
 
                 file.seek(0)
                 file.write(file_text)
